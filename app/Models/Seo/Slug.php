0f8edf9a9a8d838c8c9c64966c7b114392beb449
Tehnikus <tehnikus.ua@gmail.com>
2026-07-30 00:38:39 +0300

    Core: Move Slug model to Seo domain

--- app/Models/Seo/Slug.php
+++ app/Models/Seo/Slug.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Seo;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use App\Models\Global\Store;
+use App\Models\Global\Language;
+
+use Filament\Facades\Filament;
 
 class Slug extends Model
 {
@@ -22,21 +26,35 @@ class Slug extends Model
         'is_active' => 'boolean',
     ];
 
+    // Add store_id on save
+    protected static function booted(): void
+    {
+        static::creating(function ($model) {
+            if (blank($model->store_id)) {
+                $model->store_id = Filament::getTenant()->id;
+            }
+        });
+    }
+    
+    // Pages relaion
+    public function sluggable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+    
+    // Slug create/edit form relations
     public function store(): BelongsTo
     {
         return $this->belongsTo(Store::class);
     }
 
+    // Slug create/edit form relations
     public function language(): BelongsTo
     {
         return $this->belongsTo(Language::class);
     }
 
-    public function sluggable(): MorphTo
-    {
-        return $this->morphTo();
-    }
-
+    // Slug create/edit form relations
     public function redirectedTo(): BelongsTo
     {
         return $this->belongsTo(Slug::class, 'redirected_to_id');
