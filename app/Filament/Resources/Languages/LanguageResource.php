1ed0735ad5fd0b0ac619974f34bfd2d3deb698ac
Tehnikus <tehnikus.ua@gmail.com>
2026-07-30 02:13:19 +0300

    Admin: Update Language model

--- app/Filament/Resources/Languages/LanguageResource.php
+++ app/Filament/Resources/Languages/LanguageResource.php
@@ -7,22 +7,20 @@ use App\Filament\Resources\Languages\Pages\EditLanguage;
 use App\Filament\Resources\Languages\Pages\ListLanguages;
 use App\Filament\Resources\Languages\Schemas\LanguageForm;
 use App\Filament\Resources\Languages\Tables\LanguagesTable;
-use App\Models\Language;
-use BackedEnum;
+use App\Models\Global\Language;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-use App\Filament\Support\NavigationGroup;
+
+use App\Filament\Support\AdminMenu\NavigationItem;
+use App\Filament\Support\AdminMenu\HasCentralizedNavigation;
 
 class LanguageResource extends Resource
 {
     protected static bool $isScopedToTenant = false;
     protected static ?string $model = Language::class;
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedLanguage;
-    protected static string | \UnitEnum | null $navigationGroup = NavigationGroup::GlobalSettings;
-    protected static ?int $navigationSort = 1;
     protected static ?string $recordTitleAttribute = 'name';
+    protected static bool $isGloballySearchable = false;
 
     public static function form(Schema $schema): Schema
     {
@@ -50,24 +48,16 @@ class LanguageResource extends Resource
         ];
     }
 
-    public static function getNavigationLabel(): string
-    {
-        return __('admin.languages.navigation_label');
-    }
-
-    public static function getModelLabel(): string
-    {
-        return __('admin.languages.model_label_singular');
-    }
-
-    public static function getPluralModelLabel(): string
-    {
-        return __('admin.languages.navigation_label');
-    }
-
     // Skip global search
     public static function getGloballySearchableAttributes(): array
     {
         return [];
+    }
+
+    // Some repeating navigation methods in one place
+    use HasCentralizedNavigation;
+    protected static function getMenuConfig(): NavigationItem
+    {
+        return NavigationItem::Languages;
     }
 }
