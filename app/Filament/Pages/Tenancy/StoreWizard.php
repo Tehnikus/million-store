53b47bc424e23189dfee54f8b8b3298e35942e05
Tehnikus <tehnikus.ua@gmail.com>
2026-07-31 21:23:46 +0300

    Admin: Set initial store creation wizard width to be wider

--- app/Filament/Pages/Tenancy/StoreWizard.php
+++ app/Filament/Pages/Tenancy/StoreWizard.php
@@ -7,10 +7,13 @@ use App\Filament\Schemas\StoreRegistrationWizard;
 use Filament\Pages\Tenancy\RegisterTenant;
 use Filament\Schemas\Components\Wizard;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\Width;
 use Illuminate\Database\Eloquent\Model;
 
 class StoreWizard extends RegisterTenant
 {
+    protected Width | string | null $maxContentWidth = Width::Container; // Override login page width as this wizard is considered a login page 
+
     public static function getLabel(): string
     {
         return __('admin.global.store_wizard.navigation_label');
@@ -19,7 +22,7 @@ class StoreWizard extends RegisterTenant
     public function form(Schema $schema): Schema
     {
         return $schema->components([
-            Wizard::make(StoreRegistrationWizard::steps())->columnSpanFull(),
+            Wizard::make(StoreRegistrationWizard::steps()),
         ]);
     }
 
