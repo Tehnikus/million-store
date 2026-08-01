db6202c1253b8106957ee45d37ac90df6a85f4ea
Tehnikus <tehnikus.ua@gmail.com>
2026-08-01 03:12:28 +0300

    Admin: Rework ProducDescription model

--- app/Models/Catalog/ProductDescription.php
+++ app/Models/Catalog/ProductDescription.php
@@ -2,32 +2,22 @@
 
 namespace App\Models\Catalog;
 
-use App\Domain\Media\Concerns\HasProcessedImages;
 use Illuminate\Database\Eloquent\Model;
+// use Filament\Facades\Filament;
 use Spatie\Translatable\HasTranslations;
 
 class ProductDescription extends Model
 {
-    use HasTranslations, HasProcessedImages;
-
-    public $incrementing = false; // Disables Eloquent's auto-increment handling on insert (it would otherwise try to read back a non-existent "id" column after INSERT).
-
-    public $translatable = [
-        'name',
-        'h1',
-        'meta_title',
-        'meta_description',
-        'description_short',
-        'description_full',
-        'seo_keywords',
-        'faq',
-        'how_to',
-        'footer',
-    ];
+    use HasTranslations;
 
     protected $fillable = [
         'product_id',
         'store_id',
+        'is_active',
+        'is_available',
+        'is_available_from',
+        'is_available_to',
+        'sort_order',
         'name',
         'h1',
         'meta_title',
@@ -42,27 +32,33 @@ class ProductDescription extends Model
     ];
 
     protected $casts = [
-        'name'              => 'array',
-        'h1'                => 'array',
-        'meta_title'        => 'array',
-        'meta_description'  => 'array',
-        'images'            => 'array',
-        'description_short' => 'array',
-        'description_full'  => 'array',
-        'seo_keywords'      => 'array',
-        'faq'               => 'array',
-        'how_to'            => 'array',
-        'footer'            => 'array',
+        'is_active'             => 'boolean',
+        'is_available'          => 'boolean',
+        'is_available_from'     => 'datetime',
+        'is_available_to'       => 'datetime',
+        'sort_order'            => 'integer',
+        'name'                  => 'array',
+        'h1'                    => 'array',
+        'meta_title'            => 'array',
+        'meta_description'      => 'array',
+        'images'                => 'array',
+        'description_short'     => 'array',
+        'description_full'      => 'array',
+        'seo_keywords'          => 'array',
+        'faq'                   => 'array',
+        'how_to'                => 'array',
+        'footer'                => 'array',
     ];
-
-    // Required for image conversions
-    public function imageColumns(): array
-    {
-        return [
-            'images' => [
-                'type' => 'product',
-                'slug_source' => 'name',
-            ],
-        ];
-    }
+    protected $translatable = [
+        'name',
+        'h1',
+        'meta_title',
+        'meta_description',
+        'description_short',
+        'description_full',
+        'seo_keywords',
+        'faq',
+        'how_to',
+        'footer',
+    ];
 }
