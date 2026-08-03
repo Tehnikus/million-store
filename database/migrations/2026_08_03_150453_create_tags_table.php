3413883a9e45a115da0f869c0c41f764abae957d
Tehnikus <tehnikus.ua@gmail.com>
2026-08-03 19:13:48 +0300

    DB: Create tags table

---
+++ database/migrations/2026_08_03_150453_create_tags_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained('stores')->cascadeOnDelete();
+            // Flags
+            $table->boolean('is_active')->default(false);
+            $table->boolean('show_in_facets')->default(false);
+            $table->integer('sort_order')->default(1);
+            $table->text('inline_style')->nullable();
+            // Descriptions
+            $table->jsonb('name')->nullable()->default('{}');
+            $table->jsonb('h1')->nullable()->default('{}');
+            $table->jsonb('meta_title')->nullable()->default('{}');
+            $table->jsonb('meta_description')->nullable()->default('{}');
+            $table->jsonb('description_short')->nullable()->default('{}');
+            $table->jsonb('description_full')->nullable()->default('{}');
+            $table->jsonb('seo_keywords')->nullable()->default('{}');
+            $table->jsonb('faq')->nullable()->default('{}');
+            $table->jsonb('how_to')->nullable()->default('{}');
+            $table->jsonb('footer')->nullable()->default('{}');
+            $table->jsonb('images')->nullable()->default('{}');
+            $table->tinyText('robots')->default('noindex, nofollow');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+    }
+};
