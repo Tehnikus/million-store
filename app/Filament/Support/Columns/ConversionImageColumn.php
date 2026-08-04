49631ddb939c7fc7f2663fafd72eecfc0b7c2937
Tehnikus <tehnikus.ua@gmail.com>
2026-08-04 12:02:26 +0300

    Admin: Add custom reusable image conversions column

---
+++ app/Filament/Support/Columns/ConversionImageColumn.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Support\Columns;
+
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Support\Enums\Alignment;
+
+/**
+ * Miniature from images column with HasProcessedImages
+ * Returns conversions.miniature of the first image
+ */
+class ConversionImageColumn extends ImageColumn
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->disk('public')
+            ->imageHeight(70)
+            ->checkFileExistence(false)
+            ->alignment(Alignment::Center)
+            ->width('1%')
+            ->extraImgAttributes(['loading' => 'lazy', 'style' => 'border-radius: .5rem; margin: -0.7rem 0;'])
+            ->label(__('admin.common.fields.image'));
+    }
+
+    public function conversion(string $key): static
+    {
+        $this->state(fn ($record) => collect($record->images ?? [])
+            ->pluck("conversions.{$key}")
+            ->filter()
+            ->values()
+            ->first()
+        );
+
+        return $this;
+    }
+}
