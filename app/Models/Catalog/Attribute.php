4ab368318d96e807f3e1ea8e58e6992639911e55
Tehnikus <tehnikus.ua@gmail.com>
2026-08-04 11:11:46 +0300

    Core: Add Attribute model

---
+++ app/Models/Catalog/Attribute.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Catalog;
+
+use App\Models\Global\Store;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Translatable\HasTranslations;
+
+class Attribute extends Model
+{
+    use HasTranslations;
+    protected $fillable = [
+        'store_id',
+        'is_active',
+        'show_in_facets',
+        'sort_order',
+        'name',
+    ];
+    protected $casts = [
+        'is_active'             => 'boolean',
+        'show_in_facets'        => 'boolean',
+        'sort_order'            => 'integer',
+        'name'                  => 'array',
+    ];
+    protected $translatable = [
+        'name'
+    ];
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function values(): HasMany
+    {
+        return $this->hasMany(AttributeValue::class);
+    }
+}
