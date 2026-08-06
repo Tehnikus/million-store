64fe134bc5bc94fef32b2c30937e6b4100717d5f
Tehnikus <tehnikus.ua@gmail.com>
2026-08-06 15:26:03 +0300

    DB: Add option values table migration

---
+++ database/migrations/2026_08_06_003231_create_option_values_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('option_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('option_id')->constrained('options', 'id')->cascadeOnDelete();
+            $table->foreignId('store_id')->constrained('stores')->cascadeOnDelete();
+
+            // Flags
+            $table->boolean('is_active')->default(false);
+            $table->boolean('show_in_facets')->default(false);
+            $table->boolean('is_default')->default(false);
+            $table->integer('sort_order')->default(1);
+            // Descriptions
+            $table->jsonb('name')->nullable()->default('{}');
+            $table->jsonb('description')->nullable()->default('{}');
+            $table->jsonb('images')->nullable()->default('{}');
+            $table->tinyText('robots')->default('noindex, nofollow');
+            $table->timestamps();
+
+            $table->index(['option_id', 'store_id', 'is_active', 'sort_order']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('option_values');
+    }
+};
