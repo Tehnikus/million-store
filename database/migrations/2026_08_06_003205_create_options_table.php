e9310de0e063f53aa9b699b51ce073389a1e9c31
Tehnikus <tehnikus.ua@gmail.com>
2026-08-06 15:25:47 +0300

    DB: Add options table migration

---
+++ database/migrations/2026_08_06_003205_create_options_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained('stores')->cascadeOnDelete();
+            // Flags
+            $table->boolean('is_active')->default(false);
+            $table->boolean('show_in_facets')->default(false);
+            $table->integer('sort_order')->default(1);
+            // Descriptions
+            $table->jsonb('name')->nullable()->default('{}');
+            $table->tinyText('type')->default('radio');
+            $table->timestamps();
+
+            $table->index(['store_id', 'is_active', 'sort_order']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('options');
+    }
+};
