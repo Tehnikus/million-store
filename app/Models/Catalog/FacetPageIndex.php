f7fdbec5d6e37da3edd0eaa728335cb9dbd4cc8e
Tehnikus <tehnikus.ua@gmail.com>
2026-08-12 21:44:00 +0300

    Core: Add FacetPageIndex model for SEO Filter pages

---
+++ app/Models/Catalog/FacetPageIndex.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Catalog;
+
+use App\Domain\Support\Concerns\InheritsColumnFromParent;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class FacetPageIndex extends Model
+{
+    protected $table = 'facet_page_index';
+    protected $fillable = [
+        'facet_page_id',
+        'facet_type_id',
+        'facet_group_id',
+        'facet_value_id',
+    ];
+    protected $casts = [
+        'facet_page_id'     => 'integer',
+        'facet_type_id'     => 'integer',
+        'facet_group_id'    => 'integer',
+        'facet_value_id'    => 'integer',
+    ];
+
+    public function facetPage(): BelongsTo
+    {
+        return $this->belongsTo(FacetPage::class, 'facet_page_id');
+    }
+
+    use InheritsColumnFromParent;
+    protected static function inheritedColumns(): array
+    {
+        return [
+            'store_id' => ['facetPage', 'store_id'],
+        ];
+    }
+}
