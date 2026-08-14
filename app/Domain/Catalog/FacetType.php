9bc6b6c2675a512729358f551b989a976c376991
Tehnikus <tehnikus.ua@gmail.com>
2026-08-14 11:41:52 +0300

    Core: Extend FacetType with new facet types, add some display methods

--- app/Domain/Catalog/FacetType.php
+++ app/Domain/Catalog/FacetType.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Domain\Catalog;
+use App\Filament\Support\AdminMenu\NavigationItem;
 use Filament\Support\Contracts\HasLabel;
 
 /**
  * Facet type dictionary
+ * Used in Product create/edit/delete, all product related entities, Facet index build/cleanup, filter page table
+ * Be careful here
  */
 enum FacetType: int implements HasLabel
 {
@@ -13,7 +16,15 @@ enum FacetType: int implements HasLabel
     case Attribute     = 3;
     case Option        = 4;
     case Tag           = 5;
+    case IsFeatured    = 6;
+    case HasDiscount   = 7;
+    case Bestseller    = 8;
+    case BestReviews   = 9;
 
+    /**
+     * Get label for app\Filament\Resources\FacetPages\Tables\FacetPagesTable.php
+     * @return string
+     */
     public function getLabel(): string
     {
         return match ($this) {
@@ -22,6 +33,54 @@ enum FacetType: int implements HasLabel
             self::Attribute     => __('admin.catalog.facets.types.attribute'),
             self::Option        => __('admin.catalog.facets.types.option'),
             self::Tag           => __('admin.catalog.facets.types.tag'),
+            self::IsFeatured    => __('admin.catalog.facets.types.is_featured'),
+            self::HasDiscount   => __('admin.catalog.facets.types.has_discount'),
+            self::Bestseller    => __('admin.catalog.facets.types.bestseller'),
+            self::BestReviews   => __('admin.catalog.facets.types.best_reviews'),
+        };
+    }
+
+    /**
+     * Get icon for app\Filament\Resources\FacetPages\Tables\FacetPagesTable.php
+     * @return string
+     */
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::Category      => NavigationItem::Categories->icon(),
+            self::Manufacturer  => NavigationItem::Manufacturers->icon(),
+            self::Attribute     => NavigationItem::Attributes->icon(),
+            self::Option        => NavigationItem::Options->icon(),
+            self::Tag           => NavigationItem::Tags->icon(),
+            self::IsFeatured    => 'heroicon-o-fire',
+            self::HasDiscount   => 'heroicon-o-currency-dollar',
+            self::Bestseller    => 'heroicon-o-arrow-trending-up',
+            self::BestReviews   => 'heroicon-o-hand-thumb-up',
+            
+        };
+    }
+
+    public function getColor(): ?string
+    {
+        return match ($this) {
+            self::Category      => 'success',
+            self::Manufacturer  => 'success',
+            self::Attribute     => 'primary',
+            self::Option        => 'primary',
+            self::Tag           => 'primary',
+            self::IsFeatured    => 'info',
+            self::HasDiscount   => 'info',
+            self::Bestseller    => 'info',
+            self::BestReviews   => 'info',
+        };
+    }
+
+    public function sortPriority(): int
+    {
+        return match ($this) {
+            self::Category, self::Manufacturer        => 1,
+            self::Attribute, self::Option, self::Tag  => 2,
+            default                                   => 3,
         };
     }
 }
