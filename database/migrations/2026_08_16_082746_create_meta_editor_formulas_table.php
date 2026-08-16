d5991785ea7982fea900329c5eb01adfe9ade319
Tehnikus <tehnikus.ua@gmail.com>
2026-08-16 12:54:37 +0300

    DB: Add meta_editor_formulas table

---
+++ database/migrations/2026_08_16_082746_create_meta_editor_formulas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meta_editor_formulas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained('stores', 'id')->cascadeOnDelete();
+            $table->string('entity_type');
+            $table->string('target_field');
+            $table->string('locale');
+            $table->unsignedBigInteger('currency_id')->nullable();
+            $table->text('formula');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meta_editor_formulas');
+    }
+};
