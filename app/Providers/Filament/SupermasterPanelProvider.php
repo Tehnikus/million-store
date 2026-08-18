0c45051622ce605452bba0fdf7dff3943ebef91f
Tehnikus <tehnikus.ua@gmail.com>
2026-08-18 11:17:17 +0300

    Admin: discover clusters in PanelProvider

--- app/Providers/Filament/SupermasterPanelProvider.php
+++ app/Providers/Filament/SupermasterPanelProvider.php
@@ -45,6 +45,7 @@ class SupermasterPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+            ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\Filament\Clusters')
             ->pages([
                 Dashboard::class,
             ])
