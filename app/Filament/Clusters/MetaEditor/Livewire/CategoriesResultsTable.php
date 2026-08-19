864a6a4aebf48b736fe173b217d38487487e92f3
Tehnikus <tehnikus.ua@gmail.com>
2026-08-20 02:38:04 +0300

    MetaEditor: Remove unused code from CategoriesResultsTable, now implmented in AbstractResultsTable

--- app/Filament/Clusters/MetaEditor/Livewire/CategoriesResultsTable.php
+++ app/Filament/Clusters/MetaEditor/Livewire/CategoriesResultsTable.php
@@ -4,74 +4,13 @@ namespace App\Filament\Clusters\MetaEditor\Livewire;
 
 use App\Models\Catalog\Category;
 use Filament\Facades\Filament;
-use Filament\Support\Colors\Color;
-use Filament\Support\Enums\FontWeight;
-use Filament\Support\Enums\TextSize;
-use Filament\Tables\Columns\Layout\Grid;
-use Filament\Tables\Columns\Layout\Stack;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\TextInputColumn;
 use Illuminate\Database\Eloquent\Model;
 
 class CategoriesResultsTable extends AbstractResultsTable
 {
-
-    /**
-     * Staging table columns in AbstractResultsTable::table()
-     * TODO Maybe move columns entirely in AbstractResultsTable::table()?
-     * @return \Filament\Tables\Columns\Layout\Grid[]
-     */
-    protected function resultColumns(): array
+    protected function entityType(): string
     {
-        $languages = once(fn () => Filament::getTenant()->languages()->wherePivot('is_active', true)->pluck('locale'));
-
-        return [
-            Grid::make(['lg' => 1, 'xl' => 4])
-                ->schema([
-                    TextColumn::make('name')
-                        ->weight(FontWeight::Bold)
-                        ->color('primary')
-                        ->columnSpanFull()
-                        ->alignCenter()
-                        ->size(TextSize::Medium),
-
-                    Stack::make([
-                        TextColumn::make('column_meta_title')
-                            ->default(__('admin.common.fields.meta_title'))
-                            ->color(Color::Gray)
-                            ->size(TextSize::Small),
-                        ...collect($languages)->map(fn ($locale) => TextInputColumn::make("meta_title.{$locale}")
-                            ->getStateUsing(fn (array $record) => $record['meta_title'][$locale] ?? '')
-                            ->updateStateUsing(fn ($state, array $record) => $this->updateResultField('meta_title', $state, $record, $locale))
-                            ->prefix($locale)
-                            ->placeholder(__('admin.common.fields.meta_title'))),
-                    ])->space(2)->columnSpan(['lg' => 1, 'xl' => 1]),
-
-                    Stack::make([
-                        TextColumn::make('column_h1')
-                            ->default(__('admin.common.fields.h1'))
-                            ->color(Color::Gray)
-                            ->size(TextSize::Small),
-                        ...collect($languages)->map(fn ($locale) => TextInputColumn::make("h1.{$locale}")
-                            ->getStateUsing(fn (array $record) => $record['h1'][$locale] ?? '')
-                            ->updateStateUsing(fn ($state, array $record) => $this->updateResultField('h1', $state, $record, $locale))
-                            ->prefix($locale)
-                            ->placeholder(__('admin.common.fields.h1'))),
-                    ])->space(2)->columnSpan(['lg' => 1, 'xl' => 1]),
-
-                    Stack::make([
-                        TextColumn::make('column_description')
-                            ->default(__('admin.common.fields.meta_description'))
-                            ->color(Color::Gray)
-                            ->size(TextSize::Small),
-                        ...collect($languages)->map(fn ($locale) => TextInputColumn::make("meta_description.{$locale}")
-                            ->getStateUsing(fn (array $record) => $record['meta_description'][$locale] ?? '')
-                            ->updateStateUsing(fn ($state, array $record) => $this->updateResultField('meta_description', $state, $record, $locale))
-                            ->prefix($locale)
-                            ->placeholder(__('admin.common.fields.meta_description'))),
-                    ])->space(2)->columnSpan(['lg' => 1, 'xl' => 2]),
-                ]),
-        ];
+        return 'category';
     }
 
     /**
@@ -90,8 +29,9 @@ class CategoriesResultsTable extends AbstractResultsTable
      */
     protected function resolveEntity(int|string $id): ?Model
     {
+        $storeId = once(fn() => Filament::getTenant()->id);
         return Category::query()
-            ->where('store_id', Filament::getTenant()->id)
+            ->where('store_id', $storeId)
             ->whereKey($id)
             ->first();
     }
