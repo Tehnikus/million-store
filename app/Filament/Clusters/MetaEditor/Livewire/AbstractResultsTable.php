cc9557db977ac967df9069b9de561f7d7968c582
Tehnikus <tehnikus.ua@gmail.com>
2026-08-19 13:07:38 +0300

    MetaEditor: Add comments and minor fixes in AbstractResultsTable

--- app/Filament/Clusters/MetaEditor/Livewire/AbstractResultsTable.php
+++ app/Filament/Clusters/MetaEditor/Livewire/AbstractResultsTable.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Clusters\MetaEditor\Livewire;
 
+use App\Domain\Seo\ApplyMetaFormula;
 use Filament\Actions\Action;
+use Filament\Actions\BulkAction;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Facades\Filament;
 use Filament\Notifications\Notification;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
@@ -17,6 +20,12 @@ use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
+/**
+ * Staging table for MetaEditor.
+ * Stores rows selected for meta tag edition/generation.
+ * Rows are added by user from entitesTable
+ * No changes to DB applied until saveResults() is called
+ */
 abstract class AbstractResultsTable extends Component implements HasActions, HasSchemas, HasTable
 {
     use InteractsWithActions;
@@ -34,6 +43,7 @@ abstract class AbstractResultsTable extends Component implements HasActions, Has
     /** Resolve entites with Eloquent model */
     abstract protected function resolveEntity(int|string $id): ?Model;
 
+    /** Add row to staging table */
     #[On('meta-editor:add-to-results')]
     public function addRow(array $row): void
     {
@@ -70,8 +80,10 @@ abstract class AbstractResultsTable extends Component implements HasActions, Has
             'store' => once(fn() => Filament::getTenant()->name),
         ];
     }
+
+    protected function updateResultField(string $field, mixed $state, array $record, string $locale): void
     {
-        $this->resultsTable[$record['id']][$field][$locale] = $state;
+        $this->resultsTable[$record['id']][$field][$locale] = $state ?? '';
     }
 
     protected function removeRecordByKey(int|string $id): void
@@ -82,6 +94,7 @@ abstract class AbstractResultsTable extends Component implements HasActions, Has
     public function table(Table $table): Table
     {
         return $table
+            // Pagination
             // Uncomment this if pagination glitches FIX
             // ->records(fn () => array_values($this->resultsTable))
             // Comment this if pagination glitches
@@ -96,13 +109,20 @@ abstract class AbstractResultsTable extends Component implements HasActions, Has
                 );
             })
             ->columns($this->resultColumns())
+            // Rows background color
+            ->recordClasses(function ($record) {
+                return (!empty($record['has_error']) && $record['has_error'] == true)
+                    ? 'bg-danger-300 dark:bg-danger-600' 
+                    : 'bg-success-300 dark:bg-success-600';
+            })
+            // Row actions
             ->recordActions([
                 // TODO single generation by active formula
                 Action::make('generate')
                     ->color('info')
                     ->icon('heroicon-s-puzzle-piece')
                     ->action(fn (array $record) => null),
-
+                // Remove row from staging table
                 Action::make('delete')
                     ->color('danger')
                     ->icon('heroicon-s-no-symbol')
@@ -113,10 +133,10 @@ abstract class AbstractResultsTable extends Component implements HasActions, Has
             ])
             // Toolbar actions
             ->toolbarActions([
-                
+
                 // Save all staging changes
                 Action::make('saveAll')
-                    ->label(__('admin.common.buttons.save'))
+                    ->label(__('admin.seo.meta_editor.buttons.save_staging'))
                     ->color('success')
                     ->icon('heroicon-o-check')
                     ->action(fn () => $this->saveResults())
@@ -125,15 +145,22 @@ abstract class AbstractResultsTable extends Component implements HasActions, Has
 
                 // Clear all staging rows
                 Action::make('clearAll')
-                    ->label(__('admin.common.buttons.clear'))
+                    ->label(__('admin.seo.meta_editor.buttons.clear_staging'))
                     ->color('danger')
                     ->icon('heroicon-o-no-symbol')
                     ->action(function() {$this->resultsTable = []; $this->resetTable();})
                     ->visible(fn($records) => collect($this->resultsTable)->count() > 0)
                     ->accessSelectedRecords(),
+
+                // BulkAction::make('feature')
+                //     ->requiresConfirmation()
+                //     ->action(function ($records): void {
+                //         // Do something with the collection of `$records` data
+                //     }),
             ])
             ->emptyStateHeading(__('admin.seo.meta_editor.helpers.empty_results_table_title'))
-            ->emptyStateDescription(__('admin.seo.meta_editor.helpers.empty_results_table_descriptions'));
+            ->emptyStateDescription(__('admin.seo.meta_editor.helpers.empty_results_table_descriptions'))
+            ->emptyStateIcon('heroicon-o-clock');
     }
 
     protected function saveResults(): void
