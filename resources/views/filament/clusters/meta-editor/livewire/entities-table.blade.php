e48b28d8e95aa80d746225607d190a775f63e0f7
Tehnikus <tehnikus.ua@gmail.com>
2026-08-20 15:14:57 +0300

    MetaEditor: Add inline styles to highlight successful/faulty rows after generation

--- resources/views/filament/clusters/meta-editor/livewire/entities-table.blade.php
+++ resources/views/filament/clusters/meta-editor/livewire/entities-table.blade.php
@@ -3,3 +3,10 @@
 
     <x-filament-actions::modals />
 </div>
+
+<style>
+    .meta-editor-row-error { background-color: rgb(252 165 165 / 0.1); }
+    .dark .meta-editor-row-error { background-color: rgb(153 27 27 / 0.1); }
+    .meta-editor-row-ok { background-color: rgb(134 239 172 / 0.1); }
+    .dark .meta-editor-row-ok { background-color: rgb(21 128 61 / 0.1); }
+</style>
