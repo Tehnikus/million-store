1a9266a1c2a8f9759db64aad83883bf6bec293be
Tehnikus <tehnikus.ua@gmail.com>
2026-08-23 13:44:43 +0300

    Admin: Update create/edit pipeline of Face Pages according to FacetPageForm changes

--- app/Filament/Resources/FacetPages/Pages/CreateFacetPage.php
+++ app/Filament/Resources/FacetPages/Pages/CreateFacetPage.php
@@ -3,16 +3,25 @@
 namespace App\Filament\Resources\FacetPages\Pages;
 
 use App\Filament\Resources\FacetPages\FacetPageResource;
+use App\Filament\Resources\FacetPages\Schemas\FacetPageForm;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Concerns\StripsSlugFormState;
 
+// CreateFacetPage.php
 class CreateFacetPage extends CreateRecord
 {
     use StripsSlugFormState;
     protected static string $resource = FacetPageResource::class;
-    
+    protected ?array $pendingRootFacet = null;
+
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        return $this->stripSlugFormState($data);
+        [$data, $this->pendingRootFacet] = FacetPageForm::extractRootFacet($this->stripSlugFormState($data));
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        FacetPageForm::saveRootFacet($this->record, $this->pendingRootFacet);
     }
 }
--- app/Filament/Resources/FacetPages/Pages/EditFacetPage.php
+++ app/Filament/Resources/FacetPages/Pages/EditFacetPage.php
@@ -3,24 +3,43 @@
 namespace App\Filament\Resources\FacetPages\Pages;
 
 use App\Filament\Resources\FacetPages\FacetPageResource;
+use App\Filament\Resources\FacetPages\Schemas\FacetPageForm;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 use App\Filament\Concerns\StripsSlugFormState;
 
 class EditFacetPage extends EditRecord
 {
+    use StripsSlugFormState;
     protected static string $resource = FacetPageResource::class;
+    protected ?array $pendingRootFacet = null;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $root = $this->record->facetIndex()->where('is_root', true)->first();
+
+        if ($root) {
+            $data['root_facet_type_id']  = $root->facet_type_id->value; // explicitly scalar, as options() expects
+            $data['root_facet_value_id'] = $root->facet_value_id;
+            $data['root_facet_group_id'] = $root->facet_group_id;
+        }
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        [$data, $this->pendingRootFacet] = FacetPageForm::extractRootFacet($this->stripSlugFormState($data));
+        return $data;
+    }
+
+    protected function afterSave(): void
+    {
+        FacetPageForm::saveRootFacet($this->record, $this->pendingRootFacet);
+    }
 
     protected function getHeaderActions(): array
     {
-        return [
-            DeleteAction::make(),
-        ];
-    }
-
-    use StripsSlugFormState;
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        return $this->stripSlugFormState($data);
+        return [DeleteAction::make()];
     }
 }
