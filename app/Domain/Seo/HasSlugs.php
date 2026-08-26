db2a9368656c5c4225c68f6b126e245f3300f214
Tehnikus <tehnikus.ua@gmail.com>
2026-08-26 13:44:03 +0300

    Core: Update HasSlugs trait, remove Filament tenant store_id dependency

--- app/Domain/Seo/HasSlugs.php
+++ app/Domain/Seo/HasSlugs.php
@@ -4,31 +4,22 @@ namespace App\Domain\Seo;
 
 use App\Models\Seo\Slug;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Filament\Facades\Filament;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
-/**
- * Use when resource has URL slugs
- * Sets interaction with Slugs model and table
- */
 trait HasSlugs
 {
     public static function bootHasSlugs(): void
     {
-        static::deleting(function (self $model) {
-            $model->slugs()->delete();
-        });
+        static::deleting(fn (self $model) => $model->slugs()->delete());
     }
 
-    // Used when model record is deleted
     public function slugs(): MorphMany
     {
         return $this->morphMany(Slug::class, 'sluggable');
     }
 
-    // Used in app\Filament\Schemas\Tabs\DescriptionTab.php
-    public function currentStoreSlugs(): MorphMany
+    public function activeSlug(): MorphOne
     {
-        $tenantId = Filament::getTenant()?->id;
-        return $this->morphMany(Slug::class, 'sluggable')->where('store_id', $tenantId);
+        return $this->morphOne(Slug::class, 'sluggable')->where('is_active', true);
     }
 }
