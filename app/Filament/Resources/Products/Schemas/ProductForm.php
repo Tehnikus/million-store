8fbbd9b93ec5180bee6a84f0334813edda032577
Tehnikus <tehnikus.ua@gmail.com>
2026-09-17 19:08:34 +0300

    Product: Rework ProductForm, use new PlacementTab and OptionsTab

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -1,105 +1,83 @@
 <?php
 
 namespace App\Filament\Resources\Products\Schemas;
-use App\Filament\Schemas\Tabs\ImagesTab;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Schemas\Components\Group;
-use Filament\Schemas\Components\Tabs;
-use Filament\Schemas\Components\Tabs\Tab;
-use Filament\Schemas\Schema;
-use Filament\Facades\Filament;
-use Filament\Schemas\Components\Section;
 
-// Reusable description tabs
 use App\Filament\Schemas\Tabs\DescriptionTab;
 use App\Filament\Schemas\Tabs\FaqTab;
 use App\Filament\Schemas\Tabs\FooterTab;
 use App\Filament\Schemas\Tabs\HowToTab;
-
-use App\Filament\Resources\Products\Schemas\CategoriesTab;
-use App\Filament\Resources\Products\Schemas\ManufacturersTab;
-use App\Filament\Resources\Products\Schemas\TagsTab;
-use App\Filament\Resources\Products\Schemas\PricesTab;
-use App\Filament\Resources\Products\Schemas\OptionsTab;
+use App\Filament\Support\AdminMenu\NavigationItem;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\FusedGroup;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 
 class ProductForm
 {
     public static function configure(Schema $schema): Schema
     {
-        $store      = Filament::getTenant();
-        $languages  = $store->activeLanguages();
-        $currencies = $store->activeCurrencies();
-        $countries  = $store->activeCountries();
+        $store = Filament::getTenant();
+        $languages = $store->activeLanguages();
 
         return $schema
             ->components([
-                // Global data
-                Section::make(__('admin.catalog.products.fields.global_data'))
-                    ->description(__('admin.catalog.products.helpers.global_data'))
-                    ->schema([
-                        ...collect($languages)->map(
-                            fn($language) =>
-                            TextInput::make("global_name.{$language->locale}")
-                                ->required()
-                                ->prefix($language->locale)
-                                ->columnSpanFull()
-                                ->placeholder(__('admin.catalog.products.fields.global_name'))
-                                ->hiddenLabel()
-                                ->helperText(__('admin.catalog.products.helpers.global_name')),
-                        )->all(),
-                        TextInput::make('sku')
-                            ->label(__('admin.catalog.products.fields.sku'))
-                            ->placeholder(__('admin.catalog.products.fields.sku'))
-                            ->helperText(__('admin.catalog.products.helpers.sku')),
 
-                    ])
-                    ->collapsible()
-                    ->collapsed(fn($operation) => $operation !== 'create')
-                    ->columnSpanFull(),
-
-                Tabs::make('product')
+                Tabs::make(__('admin.common.tabs.description'))
                     ->schema([
-                        // Store-scoped and translatable data 
                         Tab::make(__('admin.common.tabs.content'))
                             ->schema([
-                                // Store-scoped data
-                                Group::make([
-                                    Toggle::make('is_active')
-                                        ->label(__('admin.catalog.products.fields.is_active'))
-                                        ->helperText(__('admin.catalog.products.helpers.is_active')),
-                                    Tabs::make('languages')
-                                        ->schema([
-                                            ...collect($languages)->map(fn($language) =>
-                                                Tab::make($language->locale)
-                                                    ->label("{$language->name}")
-                                                    ->schema([
-                                                        Tabs::make("content.{$language->locale}")
-                                                            ->schema([
-                                                                DescriptionTab::make($language, ['withSlug' => true]),
-                                                                FaqTab::make($language),
-                                                                HowToTab::make($language),
-                                                                FooterTab::make($language),
-                                                            ])
-
-                                                    ])
+                                Section::make()
+                                    ->schema([
+                                        FusedGroup::make(
+                                                collect($languages)->map(
+                                                    fn($language) =>
+                                                    TextInput::make("global_name.{$language->locale}")
+                                                        ->required()
+                                                        ->prefix($language->locale)
+                                                        ->columnSpanFull()
+                                                        ->label(__('admin.catalog.products.fields.global_name'))
+                                                        ->placeholder(__('admin.catalog.products.fields.global_name'))
+                                                        ->hiddenLabel(),
+                                                )->toArray(),
                                             )
-                                        ])
-                                ])
-                                // ->relationship(name:'storeDescription')
-                                ->statePath('description')
-                                ->columnSpanFull(),
-                            ]),
-
-                        Tab::make(PricesTab::label())->schema(PricesTab::schema($store->id, $languages, $currencies)),
-                        Tab::make(CategoriesTab::label())->schema(CategoriesTab::schema($store->id)),
-                        Tab::make(ManufacturersTab::label())->schema(ManufacturersTab::schema($store->id)),
-                        Tab::make(TagsTab::label())->schema(TagsTab::schema($store->id)),
-                        Tab::make(OptionsTab::label())->schema(OptionsTab::schema($store->id, $languages, $currencies)),
-                        Tab::make(AttributesTab::label())->schema(AttributesTab::schema($store->id, $languages)),
-                        ImagesTab::make($store, $languages, ['type' => 'product'])->statePath('description'),
+                                            ->columnSpanFull()
+                                            ->label(__('admin.catalog.products.fields.global_name'))
+                                            ->helperText(__('admin.catalog.products.helpers.global_name')),
+                                    ]),
+                                Tabs::make('languages')
+                                    ->schema([
+                                        ...collect($languages)->map(fn($language) =>
+                                            Tab::make($language->locale)
+                                                ->label("{$language->name}")
+                                                ->schema([
+                                                    Tabs::make("content.{$language->locale}")
+                                                        ->schema([
+                                                            DescriptionTab::make($language, ['withSlug' => true]),
+                                                            FaqTab::make($language),
+                                                            HowToTab::make($language),
+                                                            FooterTab::make($language),
+                                                        ])
+        
+                                                ])
+                                        )
+                                    ])
+                                    ->statePath('description'),
+                            ])
+                            ->icon(Heroicon::PencilSquare)
+                            ->label(__('admin.common.tabs.content')),
+                        PlacementTab::make($store, $languages)
+                            ->label(__('admin.catalog.products.tabs.placement'))
+                            ->icon(NavigationItem::Categories->icon()),
+                        OptionsTab::make($store, $languages)
+                            ->label(__('admin.catalog.products.tabs.options'))
+                            ->icon(NavigationItem::Options->icon())
                     ])
-                    ->columnSpanFull(),
+                    ->contained(false),
+                    
             ]);
     }
 }
