0834cd80df75e021b290fcf46464dbffe6cdb24a
Tehnikus <tehnikus.ua@gmail.com>
2026-09-24 16:57:21 +0300

    Chore: Fix IDE warning on wrong param type

--- app/Domain/Catalog/Search/ProductSearch.php
+++ app/Domain/Catalog/Search/ProductSearch.php
@@ -26,7 +26,7 @@ class ProductSearch
      * WHERE search_vector @@ to_tsquery('english', 'database & performance');
      * 
      * @param string $search
-     * @param int $search
+     * @param int $storeId
      * @return \Illuminate\Support\Collection<int, \stdClass>
      */
     public static function query(string $search = '', ?int $storeId = null): Builder
