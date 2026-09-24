d2ba6548d1deaff4b8cdfc55da7d376e22a98821
Tehnikus <tehnikus.ua@gmail.com>
2026-09-24 12:49:23 +0300

    Catalog: Update ProductOption model

--- app/Models/Catalog/ProductOption.php
+++ app/Models/Catalog/ProductOption.php
@@ -2,57 +2,31 @@
 
 namespace App\Models\Catalog;
 
-use App\Domain\Support\Concerns\InheritsColumnFromParent;
 use App\Models\Global\Store;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProductOption extends Model
 {
-    public $incrementing = true;
-    protected $table = 'product_options';
-
     protected $fillable = [
-        'product_id',
-        'option_id',
-        'store_id',
-        'sort_order',
+        'product_id', 
+        'store_id', 
+        'option_signature'
     ];
 
     protected $casts = [
-        'product_id'  => 'integer',
-        'option_id'   => 'integer',
-        'store_id'    => 'integer',
-        'sort_order'  => 'integer',
+        'product_id'       => 'integer',
+        'store_id'         => 'integer',
+        'option_signature' => 'array',
     ];
 
-    public function productOptionValues(): HasMany
+    public function product(): BelongsTo
     {
-        return $this->hasMany(ProductOptionValue::class);
+        return $this->belongsTo(Product::class);
     }
 
     public function store(): BelongsTo
     {
-        return $this->belongsTo(Store::class, 'store_id');
-    }
-
-    public function product(): BelongsTo
-{
-    return $this->belongsTo(Product::class);
-}
-
-    public function option(): BelongsTo
-    {
-        return $this->belongsTo(Option::class);
-    }
-
-    use InheritsColumnFromParent;
-
-    protected static function inheritedColumns(): array
-    {
-        return [
-            'store_id' => ['option', 'store_id'],
-        ];
+        return $this->belongsTo(Store::class);
     }
 }
