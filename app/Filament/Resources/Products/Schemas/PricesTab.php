6e513aaefbb5b3fd8bfa207281044eba86f6287f
Tehnikus <tehnikus.ua@gmail.com>
2026-09-26 18:21:19 +0300

    Products: Update prices tab appearence

--- app/Filament/Resources/Products/Schemas/PricesTab.php
+++ app/Filament/Resources/Products/Schemas/PricesTab.php
@@ -2,8 +2,8 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use App\Domain\Catalog\Actions\UpsertProduct;
 use App\Models\Customer\CustomerGroup;
-use App\Models\Catalog\Option;
 use App\Models\Catalog\OptionValue;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Repeater;
@@ -12,39 +12,69 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Components\FusedGroup;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Tabs\Tab;
-use Filament\Schemas\Components\Text;
 use Filament\Schemas\Components\Utilities\Get;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Context;
 
 class PricesTab
 {
-    public static function make($store, $currencies): Tab
+    public static function make($store, $currencies, $languages): Tab
     {
         return Tab::make('prices')
+            ->badge(fn($record) => ($count = $record?->priceTiers()->where('store_id', $store->id)->count()) ? $count : null)
             ->schema([
 
                 Repeater::make('priceTiers')
                     ->schema([
-                        Select::make('customer_group_id')
-                            ->options(fn() => static::customerGroupChoices($store->id))
-                            ->placeholder(__('admin.catalog.products.fields.everyone'))
-                            ->label(__('admin.catalog.products.fields.prices_customer_group')),
-                        Toggle::make('is_base')
-                            ->distinct()
-                            ->fixIndistinctState()
-                            ->live()
-                            ->label(__('admin.catalog.products.fields.is_base')),
-                        Toggle::make('is_discount')
-                            ->visible(fn(Get $get) => !$get('is_base'))
-                            ->label(__('admin.catalog.products.fields.is_discount')),
-                        TextInput::make('priority')->numeric()->default(1),
-                        FusedGroup::make([
-                            DateTimePicker::make('valid_from'),
-                            DateTimePicker::make('valid_until'),
-                        ]),
-                        TextInput::make('valid_quantity')->numeric()->nullable(),
+
+                        Group::make([
+
+                            // Price badge
+                            FusedGroup::make([
+                                ...collect($languages)->map(fn($language) => 
+                                    TextInput::make("name.$language->locale")
+                                        ->prefix($language->locale)
+                                        ->label(__('admin.catalog.products.fields.price_name'))
+                                        ->placeholder(__('admin.catalog.products.fields.price_name'))
+                                        ->hiddenLabel(),
+                                )
+    
+                            ])
+                            ->label(__('admin.catalog.products.fields.price_name'))
+                            ->helperText(__('admin.catalog.products.helpers.price_name')),
+    
+    
+                            Select::make('customer_group_id')
+                                ->options(fn() => static::customerGroupChoices($store->id))
+                                ->placeholder(__('admin.catalog.products.fields.everyone'))
+                                ->label(__('admin.catalog.products.fields.prices_customer_group')),
+                            Toggle::make('is_base')
+                                ->distinct()
+                                ->fixIndistinctState()
+                                ->live()
+                                ->label(__('admin.catalog.products.fields.is_base')),
+                            Toggle::make('is_discount')
+                                ->visible(fn(Get $get) => !$get('is_base'))
+                                ->label(__('admin.catalog.products.fields.is_discount')),
+                            TextInput::make('priority')->numeric()->default(1),
+                            FusedGroup::make([
+                                DateTimePicker::make('date_valid_from')
+                                    ->native(false)
+                                    ->placeholder(__('admin.catalog.products.fields.valid_from'))
+                                    ->columnSpan(1),
+                                DateTimePicker::make('date_valid_until')
+                                    ->native(false)
+                                    ->placeholder(__('admin.catalog.products.fields.valid_until'))
+                                    ->columnSpan(1),
+                            ])
+                            ->columns(2),
+                            TextInput::make('valid_quantity')->numeric()->nullable(),
+                        ])
+                        ->columns(1)
+                        ->columnSpan(1),
+
 
                         Fieldset::make('price')
                             ->label(__('admin.catalog.products.fields.prices'))
@@ -52,11 +82,16 @@ class PricesTab
                                 // Text::make('debug')->content(fn() => 'DEBUG = ' . json_encode($get('../../optionSignatures'))),
                                 ...static::priceFields($get, $currencies),
                             ])
-                            ->columnSpanFull(),
+                            ->columnSpan(1),
                     ])
                     ->addActionLabel(__('admin.catalog.products.buttons.add_price_tier'))
+                    ->columns(2)
                     ->columnSpanFull()
-                    ->minItems(1),
+                    ->defaultItems(1)
+                    ->minItems(1)
+                    ->live()
+                    ->deletable(fn (Get $get): bool => count($get('priceTiers')) > 1 )
+                   
             ]);
     }
 
